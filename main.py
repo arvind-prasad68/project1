33ad84e8dec55c768d91de88b96cc69e1f6bcf70
arvind-prasad68 <40472222+arvind-prasad68@users.noreply.github.com>
2023-11-30 21:03:37 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import kivy
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+
+class Interface(FloatLayout):
+    pass
+
+class FirstApp(App):
+    pass
+if __name__=="__main__":
+    FirstApp().run()
